d9e8d086c6d73ec31b3f81b9024714b0d99a15fe
GORELIKOV <al.gor99@mail.ru>
2024-06-03 17:08:26 +0600

    first commit

---
+++ crt_bt.py
@@ -0,0 +1,16 @@
+data = input('Введите дату:')
+task = input('Введите задачу:')
+data1 = input('Введите дату:')
+task1 = input('Введите задачу:')
+data2 = input('Введите дату:')
+task2 = input('Введите задачу:')
+
+
+dictionary = {data: task,
+              data1: task1,
+              data2: task2}
+
+
+#print(f' {data} {task} \n {data1} {task1} \n {data2} {task2}')
+
+#print(dictionary)
